2198f7598346574931613c1feb6933438c951240
monomarine <gamedev409@yandex.ru>
2024-02-17 09:59:09 +0500

    Добавьте файлы проекта.

---
+++ ArraysDemension2/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ArraysDemension2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите размер квадратного массива");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[,] array = new int[n,n]; //создание и инициализация двумерного массива
+
+            Random random = new Random();
+            
+            //заполнение и вывод массива с одновременным выделением цветом
+            //элементов на главной и побочной диагонали
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(10, 100);
+
+                    if(i==j) //если элемент лежит на главное диагонали
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write(array[i, j]+" ");
+                        Console.BackgroundColor = ConsoleColor.Black;//обязательно вернуть цвет в изначальный
+                    }
+                    else if (i==array.GetLength(1)-j-1) //если на побочной диагонали
+                    {
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.Write(array[i, j]+" ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.Write(array[i, j]+" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //цикл для смены места элементов главной и побочной диагонали
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                //проход по каждой строке с обращением к определенным элементам главной и побочной диагонали
+                int temp = array[i, i]; //во временную переменную кладется значение элемента главной диаогнали
+                array[i, i] = array[i, n-i-1]; //на место главной диагонали записывается значение элемента побочной диагонали
+                array[i, n-i-1] = temp; //на мето элемента побочной диагонали возвращается значение из временной переменной
+            }
+
+
+            Console.WriteLine("смена элементов главной и побочной диагонали");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i==j) //если элемент лежит на главное диагонали
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write(array[i, j]+" ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else if (i==array.GetLength(1)-j-1) //если на побочной диагонали
+                    {
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.Write(array[i, j]+" ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.Write(array[i, j]+" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ workWithColums/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace workWithColums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //генерируется прямоугольный массив (количество строк не равно столбцам)
+            //найти наибольший элемент  в каждом столбце!!
+            //и записать его в вектор максимальных элементов
+
+            Console.WriteLine("введите количество строк в массиве");
+            int row = Int32.Parse(Console.ReadLine()); //количество строк
+            Console.WriteLine("введите количество столбцов в массиве");
+            int column = Int32.Parse(Console.ReadLine()); // количество столбцов
+
+            int[,] array = new int[row, column]; //создание и инициализация двумерного массива
+
+            Random random = new Random();
+
+            //заполнение и вывод массива
+            for (int i = 0; i < array.GetLength(0); i++) 
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(10, 100);
+
+                        Console.Write(array[i, j]+" ");
+                   
+                }
+                Console.WriteLine();
+            }
+
+            int[] maximumOfColumns = new int[column]; // создание вектора максимальных значений по столбцам
+           
+            //поиск максимальных значений
+            //внешний цикл перебирает столбцы
+            //внутренний элементы в каждом столбце
+            for (int j = 0; j< array.GetLength(1); j++)
+            {
+                int max = Int32.MinValue; //эталон для сравнения
+                for (int i = 0; i< array.GetLength(0); i++)
+                {
+                    //получение максимального значения и завпись во временную
+                    //переменную max для дальнейшего сравнения
+                    if (array[i,j] > max)
+                    {
+                        max = array[i,j];
+                    }
+                }
+                maximumOfColumns[j] = max;
+            }
+
+            Console.WriteLine("максимальные значения в каждом столбце:");
+
+            foreach(int item in maximumOfColumns)
+                Console.Write(item+" ");
+        }
+    }
+}
